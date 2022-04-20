8f72241c7fb16c15ec7417b8a61da3e64e84768f
faiz4work <faizahmed11234@gmail.com>
2022-04-20 14:24:42 +0500

    added firebase authetication system. working fine.

--- twitter app/main.py
+++ twitter app/main.py
@@ -1,10 +1,14 @@
 import datetime
-from flask import Flask, render_template
 from google.cloud import datastore
+import google.oauth2.id_token
+from flask import Flask, render_template, request
+from google.auth.transport import requests
+
 
 app = Flask(__name__)
 
 datastore_client = datastore.Client()
+firebase_request_adapter = requests.Request()
 
 def store_time(dt):
     entity = datastore.Entity(key = datastore_client.key('visit'))
@@ -24,10 +28,20 @@ def fetch_times(limit):
 
 @app.route('/')
 def root():
+    id_token = request.cookies.get("token")
+    error_message = None
+    claims = None
+    times = None
+    if id_token:
+        try:
+            claims = google.oauth2.id_token.verify_firebase_token(id_token,
+            firebase_request_adapter)
+        except ValueError as exc:
+            error_message = str(exc)
     store_time(datetime.datetime.now())
-    times = fetch_times(5)
-    print(list(times))
-    return render_template('index.html')
+    times = list(fetch_times(10))
+    return render_template('index.html', user_data=claims, times=times, 
+                           error_message=error_message)
 
 
 if __name__ == '__main__':
---
+++ twitter app/static/app-setup.js
@@ -0,0 +1,14 @@
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDKH3y1cr062kUA7wzKD_YRVP4EIoBk4JQ",
+    authDomain: "twitterclone-347715.firebaseapp.com",
+    projectId: "twitterclone-347715",
+    storageBucket: "twitterclone-347715.appspot.com",
+    messagingSenderId: "731124797906",
+    appId: "1:731124797906:web:2465cb98cbfc364272380b"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
